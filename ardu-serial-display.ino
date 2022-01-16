7a35903c406c5d71ed5a36d7c8ab65401b3ef256
Del:Tango <alvearesolutions@gmail.com>
2022-01-16 14:34:32 -0500

    Arduino serial display sketch improvements.

--- ardu-serial-display.ino
+++ ardu-serial-display.ino
@@ -1,19 +1,26 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd = LiquidCrystal(2, 3, 4, 5, 6, 7);
+String displayMsg = "";
 
 void setup() {
     lcd.begin(16, 2);
     Serial.begin(9600);
     lcd.setCursor(1,0);
+    lcd.blink();
     lcd.print("No Data");
+    delay(500);
+    lcd.noBlink();
 }
 
 void loop() {
     delay(500);
     lcd.setCursor(1, 0);
     if (Serial.available() > 0) {
-        lcd.print(Serial.readString());
+        lcd.clear();
+        displayMsg = Serial.readString();
+        displayMsg.trim();
+        lcd.print(displayMsg);
     }
 }
 
