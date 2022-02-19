20baa75c536128e674bb40b3bcbd8602fbdff192
Del:Tango <alvearesolutions@gmail.com>
2022-02-18 20:33:32 -0500

    Bug fix in backpack python cargo lib.

--- backpack/bp_shell.py
+++ backpack/bp_shell.py
@@ -8,14 +8,14 @@ import logging
 
 from subprocess import Popen, PIPE
 
-log = logging.getLogger('')
+log = logging.getLogger('FloodGateNODE')
 
 
 def shell_cmd(command, user=None):
     log.debug('')
     log.debug('Issuing system command: ({})'.format(command))
     if user:
-        command = "su - -c \'{}\'".format(command)
+        command = "su {} -c \'{}\'".format(user, command)
     process = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
     output, errors = process.communicate()
     log.debug('Output: ({}), Errors: ({})'.format(output, errors))
