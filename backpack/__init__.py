77c8f00d6fa639972345aae15b6bb0694faaec2b
Del:Tango <alvearesolutions@gmail.com>
2022-05-14 07:20:45 -0400

    Minor bug fix in Python3 backpack package.

--- backpack/__init__.py
+++ backpack/__init__.py
@@ -7,8 +7,8 @@ from . import bp_shell
 from . import bp_convertors
 from . import bp_general
 from . import bp_threads
-from . import serial_power_link.py
-from . import spl_interpreter.py
-from . import spl_reader.py
-from . import spl_writer.py
+from . import serial_power_link
+from . import spl_interpreter
+from . import spl_reader
+from . import spl_writer
 
