5abb9958866c305d48826cc28ec19a59e1e236df
Del-Tango <alveare.solutions@gmail.com>
2022-12-07 18:25:30 +0200

    Updated python3 backpack with portable goodies.

--- backpack/__init__.py
+++ backpack/__init__.py
@@ -7,8 +7,4 @@ from . import bp_shell
 from . import bp_convertors
 from . import bp_general
 from . import bp_threads
-from . import serial_power_link
-from . import spl_interpreter
-from . import spl_reader
-from . import spl_writer
-
+from . import bp_computers
