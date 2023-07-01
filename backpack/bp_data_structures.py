e87c38700077d4e650211b10e22d53282b442074
DelTango <alvearesolutions@gmail.com>
2023-07-01 16:07:13 -0400

    Added the BST and Deque data structure implementations to Python3 backpack

--- backpack/bp_data_structures.py
+++ backpack/bp_data_structures.py
@@ -25,6 +25,40 @@ class Tree:
         self.root = None
 
 
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert_recursive(self.root, value)
+
+    def _insert_recursive(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = Node(value)
+            else:
+                self._insert_recursive(node.left, value)
+        else:
+            if node.right is None:
+                node.right = Node(value)
+            else:
+                self._insert_recursive(node.right, value)
+
+    def search(self, value):
+        return self._search_recursive(self.root, value)
+
+    def _search_recursive(self, node, value):
+        if node is None or node.value == value:
+            return node
+        if value < node.value:
+            return self._search_recursive(node.left, value)
+        return self._search_recursive(node.right, value)
+
+
 class LinkedList:
 
     def __init__(self):
@@ -85,5 +119,30 @@ class Queue:
             return self.queue.pop(0)
 
 
+class Deque:
+    def __init__(self):
+        self.items = []
 
+    def is_empty(self):
+        return len(self.items) == 0
 
+    def add_front(self, item):
+        self.items.insert(0, item)
+
+    def add_rear(self, item):
+        self.items.append(item)
+
+    def remove_front(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            raise IndexError("Deque is empty")
+
+    def remove_rear(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("Deque is empty")
+
+    def size(self):
+        return len(self.items)
